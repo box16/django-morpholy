d455dcd7f3a6b2ddefa63381d9c2b28a7d61d8cc
box16 <inoinobox16@gmail.com>
2021-01-31 11:25:36 +0900

    一旦morpholy削除

--- morpholy/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from . import views
-
-app_name = "morpholy"
-urlpatterns = [
-    path('', views.IndexView.as_view(), name='index'),
-    path('result', views.result_view, name='result'),
-]
